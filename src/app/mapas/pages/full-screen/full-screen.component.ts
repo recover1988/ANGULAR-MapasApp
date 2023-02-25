e46c220c726dff7c903a0819199937f9d6e8680c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-25 20:08:29 -0300

    punto central y acces token

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../../../environments/environment.development';
 
 @Component({
   selector: 'app-full-screen',
@@ -16,14 +15,17 @@ import { environment } from '../../../../environments/environment.development';
 export class FullScreenComponent implements OnInit {
   ngOnInit(): void {
 
-    (mapboxgl as any).accessToken = environment.mapboxToken;
     const map = new mapboxgl.Map({
       container: 'mapa', // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: [-64.18487550491221, -31.391669631352066], // starting position [lng, lat]
+      zoom: 17, // starting zoom
     });
-
+    // map.setLight({
+    //   "anchor": "viewport",
+    //   "color": "blue",
+    //   "intensity": 0.5
+    // });
 
   }
 
