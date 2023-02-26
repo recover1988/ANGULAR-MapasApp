5e72f83efd33cd856172e64a59653779f43d6305
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-26 01:44:09 -0300

    borrar y actualizar marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -67,6 +67,10 @@ export class MarcadoresComponent implements AfterViewInit {
 
     this.guardarMarcadoresLocalStorage();
 
+    nuevoMarcador.on('dragend', () => {
+      this.guardarMarcadoresLocalStorage();
+    })
+
   };
 
 
@@ -112,8 +116,24 @@ export class MarcadoresComponent implements AfterViewInit {
         marker: newMarker,
         color: m.color
       })
+
+      newMarker.on('dragend', () => {
+        this.guardarMarcadoresLocalStorage();
+      })
     })
 
+
+
+
+
+
+  }
+
+
+  borrarMarcador(i: number) {
+    this.marcadores[i].marker!.remove();
+    this.marcadores.splice(i, 1);
+    this.guardarMarcadoresLocalStorage();
   }
 
 }
